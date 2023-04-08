7203534a47555a638a43cee0df28da8aefdda053
mingyo <mingyo2410@gmail.com>
2023-04-09 00:14:35 +0800

    完成 刪除單筆/切換打勾 功能

--- all.js
+++ all.js
@@ -1,8 +1,7 @@
 const inputValue = document.querySelector(".inputValue");
-const btn = document.querySelector(".btn_add");
-const list = document.querySelector(".list");
+const addBtn = document.querySelector(".btn_add");
 let data = [];
-btn.addEventListener("click",(e) => {
+addBtn.addEventListener("click",(e) => {
     e.preventDefault();
     if(inputValue.value !== ""){
        let obj ={
@@ -17,16 +16,40 @@ btn.addEventListener("click",(e) => {
         alert("請輸入內容!")
     }
 });
+
+// 新增代辦事項
+const list = document.querySelector(".list");
 const render = () =>{
     let str = "";
     data.forEach((item) =>{
-        str += `<li>
+        str += `<li data-id="${item.id}">
                   <label class="checkbox" for="">
-                    <input type="checkbox" />
+                    <input type="checkbox" ${item.check}/>
                     <span>${item.input}</span>
                   </label>
                   <a href="#" class="delete material-symbols-outlined">close</a>
                 </li>`;
     })
     list.innerHTML = str;
-}
+};
+
+// 刪除單筆/切換打勾
+list.addEventListener("click",(e) =>{
+    let id = e.target.closest("li").getAttribute("data-id")
+    if(e.target.classList.contains("delete")){
+        e.preventDefault();
+        let index = data.findIndex((item) => item.id == id);
+        data.splice(index,1);
+    }else{
+        data.forEach((item) =>{
+            if(item.id == id){
+                if(item.check !== "checked"){
+                    item.check = "checked";
+                }else{
+                    item.check = "";
+                }
+            }
+        });
+    }
+    render();
+});
